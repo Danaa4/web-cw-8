1f8a6dc705de52ece3097ecdc068415f9f732a55
Danaa4 <Danaa4@users.noreply.github.com>
2021-01-07 23:57:42 +0300

    done

--- myscript.js
+++ myscript.js
@@ -4,11 +4,72 @@ let board = [
     ['20', '21', '22']
 ]
 
+// [`00`] [`01`] [`02`]
+// [`10`] [`11`] [`12`]
+// [`20`] [`21`] [`22`]
 
+// [`00`] [`10`] [`20`]
+// [`01`] [`11`] [`21`]
+// [`02`] [`12`] [`22`]
+
+// [`00`] [`11`] [`22`]
+// [`02`] [`11`] [`20`]
+
+let turn =`O`
 
 function press(cellNO) {
 
-    // Do stuff
+    if(turn == "O"){
+        turn="X"
+    }
+    else{
+        turn="O"
+    }
+
+  board [cellNO] =turn
+  document.getElementById(`${cellNO}`).innerText=turn
+
+if (board [`00`] == turn && board [`01`] == turn && board [`02`] == turn  ){
+  document.getElementById(`state`).innerText=`Winner `
+}
+if (board [`10`] == turn && board [`11`] == turn && board [`12`] == turn  ){
+    document.getElementById(`state`).innerText=`Winner `
+  }
+    
+  if (board [`20`] == turn && board [`21`] == turn && board [`22`] == turn  ){
+    document.getElementById(`state`).innerText=`Winner `
+  }
+   
+  if (board [`00`] == turn && board  [`10`] == turn && board [`20`] == turn  ){
+    document.getElementById(`state`).innerText=`Winner `
+
+  }
+    
+  if (board [`01`] == turn && board [`11`] == turn && board [`21`] == turn  ){
+    document.getElementById(`state`).innerText=`Winner `
+
+  } 
+  if (board [`02`] == turn && board [`12`]  == turn && board [`22`] == turn  ){
+    document.getElementById(`state`).innerText=`Winner `
+
+  } 
+  if (board [`00`] == turn && board [`11`] == turn && board  [`22`] == turn  ){
+    document.getElementById(`state`).innerText=`Winner `
+
+  }
+    
+  if (board [`02`] == turn && board [`11`] == turn && board [`20`] == turn  ){
+    document.getElementById(`state`).innerText=`Winner `
+
+
+  function myFunction() {
+    location.reload();
+  }
+  }
+  
+   
+
+  // Do stuff
     console.log(cellNO)
     
 }
